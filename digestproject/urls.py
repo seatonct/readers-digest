27ebde8847845f6dbb5b72bd51ef7ca9e1b06024
Charles Seaton <seatonct@gmail.com>
2023-11-08 14:28:50 -0600

    added categories view and fixtures

--- digestproject/urls.py
+++ digestproject/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from digestapi.views import UserViewSet, BookViewSet
+from digestapi.views import UserViewSet, BookViewSet, CategoryViewSet
 
 router = DefaultRouter(trailing_slash=False)
 router.register(r'books', BookViewSet, 'book')
+router.register(r'categories', CategoryViewSet, 'category')
 
 urlpatterns = [
     path('', include(router.urls)),
